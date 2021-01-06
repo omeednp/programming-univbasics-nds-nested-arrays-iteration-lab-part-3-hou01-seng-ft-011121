b865f3475e9564a59379f78c4ae1cdc8e37de2f8
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-06 05:22:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
   while row < src.count do
     column = 0
     while column < src[row].count do
-      if src[row][column].is_a 
+      if src[row][column].is_a? String
         final_string.join(src[row][column])
       end
       column += 1
